6412990cd1018d46e837414299b34a20a7e36752
Ryan Croucher <ryan_croucher@hotmail.com>
2022-04-28 16:47:05 +1200

    covariance tests

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import numpy as np
+from unittest import *
 
-def covariance(asset_a_returns, asset_b_returns):
+def covariance(asset_a_returns, asset_b_returns) -> float:
     """
     Inputs: Two iterables of daily returns for two assets/stocks
     Lists should be the same length (same window size)
 
-    Calculated as:
-    The sum of ((each a_i return minus average a return) * (each b_i return minus average b return))
-    over the sample size - 1
-
-    Output: the covariance of the assets as a float
+    Output: the covariance of the two assets as a float
     """
 
     try:
@@ -26,13 +23,22 @@ def covariance(asset_a_returns, asset_b_returns):
 
     asset_matrix = np.array([asset_a_returns, asset_b_returns])
 
-    return np.cov(asset_matrix)[0, 1] #anything off the main diagonal (which would be self-variance)
+    return np.cov(asset_matrix)[0, 1] #anything off the main diagonal (which would be own-variance)
 
 
 def main():
+    test = TestCase()
     a_returns = [0, 1, 2]
     b_returns = [2, 1, 0]
-    print(covariance(a_returns, b_returns))
+    test.assertAlmostEqual(covariance(a_returns, b_returns), -1)
+
+    a_returns = [1.1, 1.7, 2.1, 1.4, 0.2]
+    b_returns = [3.0, 4.2, 4.9, 4.1, 2.5]
+    test.assertAlmostEqual(covariance(a_returns, b_returns), 0.665)
+
+    a_returns = [1, 1, 1, 1, 1]
+    b_returns = [1, 1, 1, 1, 1]
+    test.assertAlmostEqual(covariance(a_returns, b_returns), 0)
 
 if __name__ == '__main__':
     main()

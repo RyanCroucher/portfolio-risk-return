ec62e7e487c25bcd070f3e5f48c84905ab0c9127
Ryan Croucher <ryan_croucher@hotmail.com>
2022-04-28 16:36:40 +1200

    covariance calculation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import numpy as np
+
 def covariance(asset_a_returns, asset_b_returns):
     """
-    Inputs: Two lists of daily returns for two assets/stocks
+    Inputs: Two iterables of daily returns for two assets/stocks
     Lists should be the same length (same window size)
 
     Calculated as:
@@ -10,11 +12,27 @@ def covariance(asset_a_returns, asset_b_returns):
     Output: the covariance of the assets as a float
     """
 
-    if len(asset_a_returns) != len(asset_b_returns) or len(asset_a_returns) == 0 or len(asset_b_returns) == 0:
-        raise ValueError(f"Length of input lists must be equal and non-zero, not {len(asset_a_returns)} and {len(asset_b_returns)}")
+    try:
+        iterator = iter(asset_a_returns)
+        iterator = iter(asset_b_returns)
+    except:
+        raise TypeError(f"Inputs should be iterables, not {type(asset_a_returns)} and {type(asset_b_returns)}")
+
+    n, m = len(asset_a_returns), len(asset_b_returns)
+
+    if n != m or n <= 1 or m <= 1:
+        raise ValueError(f"Length of input lists must be equal and >1, not {n} and {m}")
+
+
+    asset_matrix = np.array([asset_a_returns, asset_b_returns])
+
+    return np.cov(asset_matrix)[0, 1] #anything off the main diagonal (which would be self-variance)
+
 
 def main():
-    print("Hello")
+    a_returns = [0, 1, 2]
+    b_returns = [2, 1, 0]
+    print(covariance(a_returns, b_returns))
 
 if __name__ == '__main__':
     main()

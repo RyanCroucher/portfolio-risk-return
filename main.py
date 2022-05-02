fa4829d617793968f5aa08e794f78de4182f7d14
Ryan Croucher <ryan_croucher@hotmail.com>
2022-05-02 15:27:02 +1200

    Basic process fully functional

--- main.py
+++ main.py
@@ -26,7 +26,17 @@ def portfolio_asset_pairs(portfolio_assets) -> np.array:
     if len(portfolio_assets) != len(set(portfolio_assets)):
         raise ValueError(f"Input contains duplicate asset names")
 
-    return np.array(list(combinations(portfolio_assets, 2)))
+    pairs = set()
+
+    #includes self, i.e. (asset_a, asset_a) as we need the variance too
+    for asset_a_i in range(len(portfolio_assets)):
+        for asset_b_i in range(asset_a_i, len(portfolio_assets)):
+            asset_a = portfolio_assets[asset_a_i]
+            asset_b = portfolio_assets[asset_b_i]
+            pairs.add(tuple(sorted([asset_a, asset_b]))) #sorted tuple
+
+    return pairs
+    #return np.array(list(combinations(portfolio_assets, 2)))
 
 
 def covariance(asset_a_returns, asset_b_returns) -> float:
@@ -69,8 +79,9 @@ def variance(asset_returns) -> float:
 
 def portfolio_variance(weights: np.array, covariance_matrix: np.array) -> float:
     """
-    Given a portfolio of weights (a vector) for each asset and a covariance matrix,
+    Given a vector of the weights of each asset (in the portfolio) and a covariance matrix,
     we can calculate the overall variance of the portfolio which is a metric for the risk of the portfolio
+    Note that volatility/stdev is the square root of this variance
 
     Inputs:
         weights: An iterable representing a vector
@@ -79,35 +90,77 @@ def portfolio_variance(weights: np.array, covariance_matrix: np.array) -> float:
             must be shape (n, n)
         e.g.:
             weights = [
-                        0.1
-                        0.6
-                        0.3
+                        0.1     #a1
+                        0.6     #a2
+                        0.3     #a3
                         ]
             covariances = 
+                            #a1   #a2   #a3
                         [
-                            0.11, 0.3, -0.2
-                            -0.5, 1,  0.1
-                            0.1,  0.22, 0.37
+                    #a1      0.11, 0.3, -0.2
+                    #a2      -0.5, 1,  0.1
+                    #a3      0.1,  0.22, 0.37
                         ]
 
     Output:
         The overall portfolio variance
-        calculated by Wtranspose * C_mat * W
+        calculated by W^T * C_mat * W
     """
 
     if len(weights) == 0 or covariance_matrix.shape != (len(weights), len(weights)):
         raise ValueError(f"Matrix shape doesn't match given number of weights")
 
     delta = 0.01
-    weight_sum = np.sum(weights.values())
+    weight_sum = np.sum(weights)
     if abs(weight_sum - 1) > delta:
         raise ValueError(f"Weights must add to 1, not {weight_sum}")
 
-    return weights.T @ covariance_matrix @ weights
+    return (weights.T @ covariance_matrix @ weights)[0, 0] #The single element of this 'matrix'
 
+def portfolios_risk(portfolios):
+    """
+    An iterable containing Portfolios
+    A portfolio is a diction of assets where
+    An asset maps asset_name: (weight, returns)
 
+    We calculate all occuring covariances, then use asset weights + covariances
+    to calculate the risk of each portfolio
+    """
 
-def main():
+    all_assets_name_to_returns = {}
+    for portfolio in portfolios:
+        for asset_name, asset_data in portfolio.items():
+            _, asset_returns = asset_data
+            all_assets_name_to_returns[asset_name] = asset_returns
+
+    occuring_pairs = set()
+
+    for portfolio in portfolios:
+        occuring_pairs |= portfolio_asset_pairs(list(portfolio.keys()))
+
+    all_covariances = {(a, b): covariance(all_assets_name_to_returns[a], all_assets_name_to_returns[b]) for a, b in occuring_pairs}
+
+    #create weight vector and covariance matrix for each portfolio
+    for portfolio in portfolios:
+        sorted_asset_names = sorted(portfolio.keys())
+        weight_vector = np.empty((len(sorted_asset_names), 1))
+        cov_matrix = np.empty((len(sorted_asset_names), len(sorted_asset_names)))
+        for i, name in enumerate(sorted_asset_names):
+            weight_vector[i, 0] = portfolio[name][0]
+            for j in range(len(sorted_asset_names)):
+                asset_a = sorted_asset_names[i]
+                asset_b = sorted_asset_names[j]
+                if asset_a > asset_b:
+                    asset_a, asset_b = asset_b, asset_a #swap them to keep alphabetical order
+                cov_matrix[i, j] = all_covariances[(asset_a, asset_b)]
+
+        print(weight_vector)
+        print(cov_matrix)
+
+        print("Volatility: ", portfolio_variance(weight_vector, cov_matrix) ** 0.5)
+
+    
+def test_covariance():
     test = TestCase()
     a_returns = [0, 1, 2]
     b_returns = [2, 1, 0]
@@ -121,7 +174,22 @@ def main():
     b_returns = [1, 1, 1, 1, 1]
     test.assertAlmostEqual(covariance(a_returns, b_returns), 0)
 
+def test_asset_pairs():
     print(portfolio_asset_pairs(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']))
+
+def main():
+    portfolio_a = {}
+    portfolio_a['asset_a'] = (0.1, [1, 3, 4, 1, -6, -1, 1, -1, 2])
+    portfolio_a['asset_b'] = (0.6, [2, 2, 3, 0, 0, -1, -1, 2, 2])
+    portfolio_a['asset_c'] = (0.3, [-1, -1, 1, 1, 0, 1, -1, 1, 2])
+
+    portfolios_risk([portfolio_a])
+
+    portfolio_b = {}
+    portfolio_b['asset_a'] = (0.5, [1, 2, 3, 4, 5, -1, -2, -3])
+    portfolio_b['asset_b'] = (0.5, [-1, -2, -3, -4, -5, 1, 2, 3])
+
+    portfolios_risk([portfolio_b])
 
 if __name__ == '__main__':
     main()
